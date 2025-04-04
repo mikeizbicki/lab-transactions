2b1ece0e482634e18145105e103c36e20ef70f91
Mike Izbicki <mike@izbicki.me>
2025-04-04 10:02:11 -0700

    bugfix

--- Ledger/__init__.py
+++ Ledger/__init__.py
@@ -75,18 +75,18 @@ class Ledger:
         '''
 
         # insert the transaction
-        sql = f'INSERT INTO transactions (debit_account_id, credit_account_id, amount) VALUES ({debit_account_id}, {credit_account_id}, {amount})'
+        sql = text(f'INSERT INTO transactions (debit_account_id, credit_account_id, amount) VALUES ({debit_account_id}, {credit_account_id}, {amount})')
         logging.debug(sql)
         self.connection.execute(sql)
 
         # update the debit account balance
-        sql = f'SELECT balance FROM balances WHERE account_id = {debit_account_id}'
+        sql = text(f'SELECT balance FROM balances WHERE account_id = {debit_account_id}')
         logging.debug(sql)
         results = self.connection.execute(sql)
         debit_account_balance = results.first()[0]
 
         debit_new_balance = debit_account_balance - amount
-        sql = f'UPDATE balances SET balance={debit_new_balance} WHERE account_id = {debit_account_id}'
+        sql = text(f'UPDATE balances SET balance={debit_new_balance} WHERE account_id = {debit_account_id}')
         logging.debug(sql)
         self.connection.execute(sql)
 
